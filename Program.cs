c87aaf60be2b28f702db989a5a21c11bdd0c0620
TechnoLatopumi <tegartpp@gmail.com>
2022-08-04 10:41:05 +0700

    mad-lib done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string color, pluralNoun, name ;
+        Console.Write("Enter a color: ");
+        color = Console.ReadLine();
+
+        Console.Write("Enter a plural noun: ");
+        pluralNoun = Console.ReadLine();
+
+        Console.Write("Enter a name: ");
+        name = Console.ReadLine();
+
+        Console.WriteLine("Roses are "+ color);
+        Console.WriteLine(pluralNoun +" are blue");
+        Console.WriteLine("I love "+ name);
+
+        Console.ReadLine();
+    }
+}
